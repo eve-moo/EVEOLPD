bc32fbde47d93e7fa79643d16ce91ac1cb22616a
Cometo <comet0@users.noreply.github.com>
2016-03-13 14:23:31 +0000

    Initial commit, slightly buggy

---
+++ README.md
@@ -0,0 +1,20 @@
+# EVE Online Live packet dumper (EVEOLPD)
+
+#### Requirements
+* Windows (Wine version in dev)
+* Visual Studio (I use 2013)
+* Some knowledge
+
+#### How to use  
+1. We need to re-enable local dll loading in windows
+  * `$ reg add "HKLM\SOFTWARE\Microsoft\WindowsNT\CurrentVersion\Image File Execution Options" /v DevOverrideEnable /t REG_DWORD /d 1`  
+2.  Download and build for release this repo
+3. Create an empty file in the eve/bin folder called `exefile.exe.local`
+4. Copy advapi32.dll to eve/bin as advapi32_.dll
+  * x86 `C:\Windows\System32\advapi32.dll`
+  * x64 `C:\Windows\SysWOW64\advapi32.dll`
+5. Copy advapi32.dll from this repo's advapi32/Release folder to eve/bin
+6. Build the control 'app' and direct it to the eve/bin folder
+7. Run exefile.exe and once loaded fully enable both incomming and outgoing packet logging
+
+Packets will be output to eve/bin/packets_{time_since_epoch_at_eve_start}
---
+++ advapi32_controller/advapi32_controller/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+ * Buggy as hell, likes to lock files while being used.
+ * If you can do better, feel free to do it.
+ * I will gladly use it and promote it.
+ */
+
+namespace advapi32_controller
+{
+    public partial class Form1 : Form
+    {
+        String EVE_DIR = null;
+        bool cryptDecrypt = false;
+        bool cryptEncrypt = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (cryptDecrypt != cryptEncrypt)
+            {
+                // Out of sequence
+                // Just disable both just incase
+                if (File.Exists(EVE_DIR + "\\advapi32_config_dump_cryptDecrypt"))
+                {
+                    if (new FileInfo(EVE_DIR + "\\advapi32_config_dump_cryptDecrypt").Length == 0)
+                    {
+                        File.Delete(EVE_DIR + "\\advapi32_config_dump_cryptDecrypt");
+                        button2.BackColor = default(Color);
+                        cryptDecrypt = false;
+                    }
+                }
+                if (File.Exists(EVE_DIR + "\\advapi32_config_dump_cryptEncrypt"))
+                {
+                    if (new FileInfo(EVE_DIR + "\\advapi32_config_dump_cryptEncrypt").Length == 0)
+                    {
+                        File.Delete(EVE_DIR + "\\advapi32_config_dump_cryptEncrypt");
+                        button3.BackColor = default(Color);
+                        cryptEncrypt = false;
+                    }
+                }
+            }
+            else if (cryptDecrypt && cryptEncrypt)
+            {
+                // enabled, now disable
+                if (File.Exists(EVE_DIR + "\\advapi32_config_dump_cryptDecrypt"))
+                {
+                    if (new FileInfo(EVE_DIR + "\\advapi32_config_dump_cryptDecrypt").Length == 0)
+                    {
+                        File.Delete(EVE_DIR + "\\advapi32_config_dump_cryptDecrypt");
+                        button2.BackColor = default(Color);
+                        cryptDecrypt = false;
+                    }
+                }
+                if (File.Exists(EVE_DIR + "\\advapi32_config_dump_cryptEncrypt"))
+                {
+                    if (new FileInfo(EVE_DIR + "\\advapi32_config_dump_cryptEncrypt").Length == 0)
+                    {
+                        File.Delete(EVE_DIR + "\\advapi32_config_dump_cryptEncrypt");
+                        button3.BackColor = default(Color);
+                        cryptEncrypt = false;
+                    }
+                }
+            }
+            else if (!cryptDecrypt && !cryptEncrypt)
+            {
+                // disabled, now enable
+                if (!File.Exists(EVE_DIR + "\\advapi32_config_dump_cryptDecrypt"))
+                {
+                    File.Create(EVE_DIR + "\\advapi32_config_dump_cryptDecrypt");
+                    button2.BackColor = Color.LightGreen;
+                    cryptDecrypt = true;
+                }
+                else
+                {
+                    MessageBox.Show("ERROR, cryptDecrypt file exists!");
+                }
+
+                if (!File.Exists(EVE_DIR + "\\advapi32_config_dump_cryptEncrypt"))
+                {
+                    File.Create(EVE_DIR + "\\advapi32_config_dump_cryptEncrypt");
+                    button3.BackColor = Color.LightGreen;
+                    cryptEncrypt = true;
+                }
+                else 
+                {
+                    MessageBox.Show("ERROR, cryptEncrypt file exists!");
+                }
+            }
+            //both
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (cryptDecrypt)
+            {
+                // disable
+                if (File.Exists(EVE_DIR + "\\advapi32_config_dump_cryptDecrypt"))
+                {
+                    if (new FileInfo(EVE_DIR + "\\advapi32_config_dump_cryptDecrypt").Length == 0)
+                    {
+                        File.Delete(EVE_DIR + "\\advapi32_config_dump_cryptDecrypt");
+                        button2.BackColor = default(Color);
+                        cryptDecrypt = false;
+                    }
+                }
+            }
+            else
+            {
+                // enable
+                if (!File.Exists(EVE_DIR + "\\advapi32_config_dump_cryptDecrypt"))
+                {
+                    File.Create(EVE_DIR + "\\advapi32_config_dump_cryptDecrypt");
+                    button2.BackColor = Color.LightGreen;
+                    cryptDecrypt = true;
+                }
+                else
+                {
+                    MessageBox.Show("ERROR, cryptDecrypt file exists!");
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (cryptEncrypt)
+            {
+                // disable
+                if (File.Exists(EVE_DIR + "\\advapi32_config_dump_cryptEncrypt"))
+                {
+                    if (new FileInfo(EVE_DIR + "\\advapi32_config_dump_cryptEncrypt").Length == 0)
+                    {
+                        File.Delete(EVE_DIR + "\\advapi32_config_dump_cryptEncrypt");
+                        button3.BackColor = default(Color);
+                        cryptEncrypt = false;
+                    }
+                }
+            }
+            else
+            {
+                // enable
+                if (!File.Exists(EVE_DIR + "\\advapi32_config_dump_cryptEncrypt"))
+                {
+                    File.Create(EVE_DIR + "\\advapi32_config_dump_cryptEncrypt");
+                    button3.BackColor = Color.LightGreen;
+                    cryptEncrypt = true;
+                }
+                else
+                {
+                    MessageBox.Show("ERROR, cryptEncrypt file exists!");
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // select eve dir
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if (File.Exists(fbd.SelectedPath + "\\advapi32.dll"))
+                {
+                    button4.Enabled = false;
+                    EVE_DIR = fbd.SelectedPath;
+                    button1.Enabled = true;
+                    button2.Enabled = true;
+                    button3.Enabled = true;
+                    if (File.Exists(EVE_DIR + "\\advapi32_config_dump_cryptDecrypt"))
+                    {
+                        button2.BackColor = Color.LightGreen;
+                        cryptDecrypt = true;
+                    }
+                    if (File.Exists(EVE_DIR + "\\advapi32_config_dump_cryptEncrypt"))
+                    {
+                        button3.BackColor = Color.LightGreen;
+                        cryptEncrypt = true;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("advapi32.dll not found in given dir.");
+                }
+            }
+        }
+    }
+}
