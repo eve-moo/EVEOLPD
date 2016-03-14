627a17f2c250b9cbbfe64dc3d67038b45e413ced
Cometo <comet0@users.noreply.github.com>
2016-03-14 10:04:50 +0000

    Auto decompyle python sent over the network.

--- decompress.py
+++ decompress.py
@@ -1,8 +1,9 @@
 import os
 import zlib
 from shutil import copyfile
+import uncompyle2
 
-PACKET_DIR = "C:\\EVE\\bin\\packets_1457914730\\"
+PACKET_DIR = "C:\\EVE\\bin\\packets_\\"
 
 for i in os.listdir(PACKET_DIR):
     with open(PACKET_DIR + i, 'rb') as f:
@@ -15,7 +16,11 @@ for i in os.listdir(PACKET_DIR):
         raw_file = open(PACKET_DIR + i + '.eve', 'wb')
         raw_file.write(raw)
         raw_file.close()
+        if ord(raw[0]) == 3:
+            uncompyle2.uncompyle_file(PACKET_DIR + i + '.eve', open(PACKET_DIR + i + '.eve.py', 'w'))
     except zlib.error:
         print "zlib.error", i
     except:
-        print "unknown except"
+        print "unknown except!"
+        import sys
+        print sys.exc_info()
